a7a43f8bee7bdbfaa9b08a83b3267e5099b17f3d
khomotsosonti Joseph <106885050+02212@users.noreply.github.com>
2023-06-07 13:11:58 +0200

    Update euler_pract_3.py
    added comments to try and explain the solution

--- euler_pract_3.py
+++ euler_pract_3.py
@@ -1,10 +1,10 @@
 def findLagestPrime(number):
     index = 2
     while index * index < number:
-        if number % index:
-            index =  index +1
+        if number % index: #check if true or false, if its true it will incement the diviser 
+            index =  index +1 # it sums the divisers together
         else:
-            number = number / index
+            number = number / index # decreases the value of the sentinel so that it will not be an infinite loop
     return number
 
 #find largest prime  number of any prime number 
